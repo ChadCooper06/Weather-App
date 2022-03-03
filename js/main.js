2f9892a280f31eaaaa9f46197e27643ac22f1221
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-03 19:50:39 +0000

    working code now

--- js/main.js
+++ js/main.js
@@ -2,97 +2,131 @@
 //setting variables that I can use through the whole app
 let appId = '60e9a913bbf1ad88bff19ed3e1d772c3';
 
-let result;
+//let result;
 let condition;
 let description;
-let city;
+let locale;
 let tempK; 
 let tempC = tempK-273.15; 
 let tempF = tempC*(9/5)+32;
-let userZip = 40517; 
+
+let userZip = 90210; 
+let resp;
 
 
 
-let weatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${userZip},us&appid=${appId}`;
+async function getWeather(){
 
+    request = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${userZip},us&appid=${appId}`);
+        let resp = request.data;
+        console.log(resp);
+        //let weather = resp.data;
+        //console.log(weather);
+        displayWeather(resp)
+}
 
+function displayWeather(resp) {
+    //console.log(resp);
+    let location = resp.name;
+    let tempK = resp.main.temp;
+    let tempC = Math.round(tempK -273);
+    let tempF = Math.round(9/5*tempC+32);
+    let condition = resp.weather[0].main;
+    let description = resp.weather[0].description;
 
-async function getWeather() {
-  try{
-      let response = await axios.get(weatherLink)
-      //console.log(response);
-      weatherObject = await response.json;
-      city = response.data.name
-      tempK = response.data.main.temp;
-      description = response.data.weather[0].description;
-      condition = response.data.weather[0].main;
-      pushText();
-      
-    } catch (error) {
-        console.error('error')
+    const locale = document.querySelector('#locale')
+    locale.textContent = `${location}`;
+    const tempKel = document.querySelector('#tempK')
+    tempKel.textContent = `${tempK} ${'K'}`;
+    const tempCel = document.querySelector('#tempC')
+    tempCel.textContent = `${tempC} ${'C'}`;
+    const tempFar = document.querySelector('#tempF')
+    tempFar.textContent = `${tempF} ${'F'}`;
+    const currentWeather= document.querySelector('#condition');
+    currentWeather.textContent = `${condition}`;
+    const descrip = document.querySelector('#description');
+    descrip.textContent = `${description}`;
+    
+    
+    //let condtion = document.getElementById("condition");
+    //let description = document.getElementById("description");
+    
+    //location.innerText = location
+}
+
+function checkZip(){
+    let zipInput = document.getElementById("zipInput");
+    if(!isNaN(zipInput.value) && zipInput.value.length === 5){
+        userZip = zipInput.value
+        getData()
+        .catch(err => {
+            return new err ("OOPS! Enter a valid ZIP")
+        });
     }
 }
-    
 
-
-function pushText() {
-  let userCity = document.getElementById("city")
-  userCity.textContent =  city;
-  let tempKelvin = document.getElementById("kelvin")
-  tempKelvin.textContent = Math.floor(tempK) + ' K'
-  let tempFaren = document.getElementById("farenheit")
-  tempFaren.textContent = Math.floor((tempK-273.15)*(9/5)+32)+ ' F'
-  let tempCels = document.getElementById('celsius')
-  tempCels.textContent = Math.floor(tempK-273.15) + ' C';
-  let newCondition= document.getElementById("condition");
-  newCondition.textContent = condition;
-  let otherInfo = document.getElementById("other");
-  otherInfo.innerText = description
-}
-
-
+function clearInput() {
+        onclick.document.getElementById("weatherBtn").value="";
+    }
+getWeather();
 
 /*
-async function getWeather() {
-   let result = await axios.get(weatherLink);
-
-    setState(result);
+    .then(resp => {
+        return resp.data;
+    }).then (data => {
+        return data
+    })
+    .catch(err => {
+        console.log(err);
+    })
 }
+*/
 
-function setState(result) {
- 
-    displayWeather()
-}
+//function setState(){
+//    displayWeather();
+//}
 
+//async function getWeather(weatherLink) {
+//    let result = await axios.get(weatherLink);
+//    let weather = await result.data;
+//}
 
+/*
 function displayWeather() {
+    const location = data.name;
+    const tempK = data.temp;
+    const condition = data.weather[0].main;
+    const description = data.weather[0].description;
     let locale = document.getElementById('locale')
-    locale.textContent = locale;
-    let tempK = document.getElementById('tempK')
-    tempK.textContent = Math.floor(tempK) + ' K'
+    locale.textContent = location;
+    let tempKel = document.getElementById('tempK')
+    tempKel.textContent = Math.floor(tempK) + ' K'
     let tempC = document.getElementById('tempC')
     tempC.textContent = Math.floor(tempK-273.15) + ' C';
     let tempF = document.getElementById('tempF')
     tempF.textContent = Math.floor((tempK-273.15)*(9/5)+32)+ ' F'
-    let condition= document.getElementById('condition');
-    condition.textContent = condition;
-    let description = document.getElementById('description');
-    otherInfo.innerText = description
+    let currentWeather= document.getElementById('condition');
+    currentWeather.textContent = condition;
+    let other = document.getElementById('description');
+    other.innerText = description
 }
+*/
 
-
-
+/*
 async function getWeather() {
     try{
     let data = await axios.get(weatherLink);
     return data;
-    let currentWeather = await data;
-    setState(response)
     } catch(err) {
         display.error('Oops! Something went wrong')
     }
 }
+*/
 
+    
+
+
+/*
 //this checks the zip to see if it is correct format
 function checkZip(value) {
     let zipPattern = /^\d{5}$/;
@@ -102,10 +136,15 @@ function checkZip(value) {
     return zipPattern.test(value);
     
 }
-//function buildPage() {
-    //    document.getElementById("weatherBtn").onclick
-    //}
+*/
+
+
     
-    function clearInput() {
-        document.getElementById("weatherBtn").value="";
-    }*/
+//    document.querySelector(".weatherBtn").addEventListener("click", function(){
+ //   weather.search();
+//});
+//document.querySelector("zipInput").addEventListener("keyup", function(event){
+//    if (event.key == "Get Weather") {
+//        weather.search();
+//    }
+//});
